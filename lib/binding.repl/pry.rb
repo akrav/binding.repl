08007bd76429b5ac744afae849c1eafd781fdfa2
Robert Gleeson <rob@flowof.info>
2013-09-29 02:00:32 +0200

    don't need public_send() here. thanks @rf-.

--- lib/binding.repl/pry.rb
+++ lib/binding.repl/pry.rb
@@ -2,6 +2,6 @@ predicate = lambda do
   defined?(Pry)
 end
 runner = lambda do |binding, options|
-  binding.public_send :pry, options
+  binding.pry(options)
 end
 Binding.repl.add :pry, predicate, runner
