294d23d6d51ecbcdf3b158a9855ae5bd70d0ec79
Robert Gleeson <rob@flowof.info>
2013-09-29 04:43:59 +0200

    disable .binding.repl.rc load with ENV["BINDING_REPL_RC"] = 0.

--- lib/binding.repl.rb
+++ lib/binding.repl.rb
@@ -89,19 +89,6 @@ require_relative "binding.repl/pry"
 require_relative "binding.repl/irb"
 require_relative "binding.repl/ripl"
 
-home_rc = File.join ENV["HOME"], ".binding.repl.rc"
-local_rc = File.join Dir.getwd, ".binding.repl.rc"
-require "json"
-if File.exists?(home_rc)
-  json = File.read(home_rc)
-end
-if !File.exists?(home_rc) && File.exists?(local_rc)
-  json = File.read(local_rc)
-end
-
-if local_rc || home_rc
-  options = JSON.parse(json) rescue nil
-end
-if options
-  Binding.repl.auto_load_order = options["auto_load_order"]
+if ENV["BINDING_REPL_RC"].to_i != 0
+  reuqire_relative "binding.repl/rc"
 end
---
+++ lib/binding.repl/rc.rb
@@ -0,0 +1,17 @@
+require "json"
+home_rc = File.join ENV["HOME"], ".binding.repl.rc"
+local_rc = File.join Dir.getwd, ".binding.repl.rc"
+
+if File.exists?(home_rc)
+  json = File.read(home_rc)
+end
+if !File.exists?(home_rc) && File.exists?(local_rc)
+  json = File.read(local_rc)
+end
+
+if local_rc || home_rc
+  options = JSON.parse(json) rescue nil
+end
+if options
+  Binding.repl.auto_load_order = options["auto_load_order"]
+end
