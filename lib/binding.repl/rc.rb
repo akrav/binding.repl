4d422e96b481dcd7ec3ece9c0e8c709bb9389eba
Robert Gleeson <rob@flowof.info>
2013-10-01 04:11:26 +0200

    fix bugs in RC loader

--- lib/binding.repl/rc.rb
+++ lib/binding.repl/rc.rb
@@ -1,19 +1,17 @@
 require "json"
 module Binding.repl::RC
   module_function
+  def home_rc
+    File.join ENV["HOME"], ".binding.repl.rc"
+  end
+
   def safe_load
-    rc = JSON.parse File.read(home_rc)
+    rc = JSON.parse File.read(home_rc) if File.exists?(home_rc)
   rescue StandardError => e
-    warn "error reading JSON file '#{home_rc}' (#{e.class}: #{e.message})"
+    warn "binding.repl: '#{home_rc}' read failed (#{e.class}): #{e.message}"
   else
     load_order = rc["auto_load_order"]
     Binding.repl.auto_load_order = load_order
   end
-
-  def home_rc
-    File.join ENV["HOME"], ".binding.repl.rc"
-  end
 end
-if File.exists?(home_rc)
-  Binding.repl::RC.safe_load
-end
+Binding.repl::RC.safe_load
