f14ddee5d81fb06bc208612875ed14fb9a0cb784
Robert Gleeson <rob@flowof.info>
2013-10-01 03:51:15 +0200

    add Binding.repl::RC.

--- lib/binding.repl/rc.rb
+++ lib/binding.repl/rc.rb
@@ -1,18 +1,18 @@
 require "json"
-module Binding.repl::Tryable
-  def try
+module Binding.repl::RC
+  def safe_load
     rc = yield
   rescue StandardError => e
     warn "error reading JSON file $HOME/.binding.repl.rc (#{e.class}: #{e.message})"
   else
     Binding.repl.auto_load_order = rc["auto_load_order"]
   end
-  module_function :try
+  module_function :safe_load
 end
 
 home_rc = File.join ENV["HOME"], ".binding.repl.rc"
 if File.exists?(home_rc)
-  Binding.repl::Tryable.try do
+  Binding.repl::RC.safe_load do
     JSON.parse File.read(home_rc)
   end
 end
